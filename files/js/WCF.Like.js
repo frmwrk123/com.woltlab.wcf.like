2e58d60967473c5799c98889ededea2052a03407
Alexander Ebert <ebert@woltlab.com>
2011-10-21 18:13:37 +0200

    Changed API to support different containers

--- files/js/WCF.Like.js
+++ files/js/WCF.Like.js
@@ -5,15 +5,19 @@
  * @copyright	2001-2011 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-WCF.Like = function() { this.init(); }
-WCF.Like.prototype = {
+WCF.Like = Class.extend({
 	/**
-	 * list of likeable objects
-	 * 
-	 * @var	jQuery
+	 * list of containers
+	 * @var	object
 	 */
-	_containers: null,
-	
+	_containers: { },
+
+	/**
+	 * container meta data
+	 * @var	object
+	 */
+	_containerData: { },
+
 	/**
 	 * proxy object
 	 * 
@@ -25,16 +29,74 @@ WCF.Like.prototype = {
 	 * Initializes like support.
 	 */
 	init: function() {
-		this._containers = $('.likeButtonContainer');
-		
-		var options = {
-			success: $.proxy(this._success, this)
+		var $containers = this._getContainers();
+		if ($containers.length === 0) {
+			console.debug("[WCF.Like] Empty container set given, aborting.");
+			return;
 		}
-		this._proxy = new WCF.Action.Proxy(options);
-		
-		this._containers.each($.proxy(function(index, container) {
-			$(container).find('span').bind('click', $.proxy(this._click, this));
+
+		$containers.each($.proxy(function(index, container) {
+			// set container
+			var $container = $(container);
+			var $containerID = $container.wcfIdentify();
+			this._containers[$containerID] = $container;
+
+			// set container data
+			this._containerData[$containerID] = {
+				'cumulativeLikes': $container.data('like-cumulativeLikes'),
+				'objectType': $container.data('objectType'),
+				'objectID': this._getObjectID($containerID)
+			};
+
+			// create UI
+			this._createButtons($containerID);
 		}, this));
+
+		this._proxy = new WCF.Action.Proxy({
+			success: $.proxy(this._success, this)
+		});
+	},
+
+	/**
+	 * Returns a list of available object containers.
+	 * 
+	 * @return	jQuery
+	 */
+	_getContainers: function() { },
+
+	/**
+	 * Returns button container for target object container.
+	 * 
+	 * @param	string		containerID
+	 * @return	jQuery
+	 */
+	_getButtonContainer: function(containerID) { },
+
+	/**
+	 * Returns object id for targer object container.
+	 * 
+	 * @param	string		containerID
+	 * @return	integer
+	 */
+	_getObjectID: function(containerID) { },
+
+	/**
+	 * Creates buttons for like and dislike.
+	 * 
+	 * @param	integer		containerID
+	 */
+	_createButtons: function(containerID) {
+		var $buttonContainer = this._getButtonContainer(containerID);
+		
+		var $listItem = $('<li class="likeButtons"></li>').data('containerID', containerID).appendTo($buttonContainer);
+		var $buttonLike = $('<img src="' + WCF.Icon.get('wcf.icon.like') + '" alt="" />').appendTo($listItem);
+		var $cumulativeLikes = $('<span>' + this._containers[containerID].data('like-cumulativeLikes') + '</span>').appendTo($listItem);
+		var $buttonDislike = $('<img src="' + WCF.Icon.get('wcf.icon.dislike') + '" alt="" />').appendTo($listItem);
+
+		$buttonLike.data('type', 'like').click($.proxy(this._click, this));
+		$buttonDislike.data('type', 'dislike').click($.proxy(this._click, this));
+
+		return $listItem;
 	},
 	
 	/**
@@ -43,37 +105,29 @@ WCF.Like.prototype = {
 	 * @param	object		event
 	 */
 	_click: function(event) {
-		var $target = $(event.target);
-		var $container = $target.parent();
+		var $button = $(event.target);
+		var $buttonContainer = $button.parent();
 		
-		var $action = $target.data('action');
-		var $isDislike = ($target.data('type') == 'dislike') ? 1 : 0;
-		var $objectType = $container.data('objectType');
-		var $objectID = $container.data('objectID');
-		
-		//this._proxy.showSpinner($target);
-		this._sendRequest($objectID, $action, $objectType, $isDislike);
+		this._sendRequest($buttonContainer.data('containerID'), $button.data('type'));
 	},
 	
 	/**
 	 * Sends request through proxy.
 	 * 
-	 * @param	integer		objectID
-	 * @param	string		action
-	 * @param	string		objectType
-	 * @param	boolean		isDislike
+	 * @param	integer		containerID
+	 * @param	string		type
 	 */
-	_sendRequest: function(objectID, action, objectType, isDislike) {
+	_sendRequest: function(containerID, type) {
 		this._proxy.setOption('data', {
-			actionName: action,
+			actionName: type,
 			className: 'wcf\\data\\like\\LikeAction',
 			parameters: {
 				data: {
-					isDislike: isDislike,
-					objectType: objectType
+					containerID: containerID,
+					objectID: this._containerData[containerID].objectID,
+					objectType: this._containerData[containerID].objectType
 				}
-			},
-			objectIDs: [ objectID ]
+			}
 		});
 		
 		this._proxy.sendRequest();
@@ -87,25 +141,30 @@ WCF.Like.prototype = {
 	 * @param	object		jqXHR
 	 */
 	_success: function(data, textStatus, jqXHR) {
-		// update items
-		this._containers.each(function(index, container) {
-			var $container = $(container);
-			if (WCF.inArray($container.data('objectID'), data.objectIDs)) {
-				var $buttonPlus = $container.children('span.likeButtonPlus');
-				var $buttonMinus = $container.children('span.likeButtonMinus');
-				
-				// update stats
-				$buttonPlus.removeClass('active').text(data.returnValues.likes);
-				$buttonMinus.removeClass('active').text(data.returnValues.dislikes);
-				
-				// mark buttons as active
-				if (data.returnValues.isLiked) {
-					$buttonPlus.addClass('active');
-				}
-				else if (data.returnValues.isDisliked) {
-					$buttonMinus.addClass('active');
-				}
-			}
-		});
+		if (!this._containers[data.returnValues.containerID]) {
+			return;
+		}
+		
+		// update container data
+		var $container = this._containers[data.returnValues.objectType][data.returnValues.objectID];
+		$container.data('likes', data.returnValues.likes);
+		$container.data('dislikes', data.returnValues.dislikes);
+		$container.data('cumulativeLikes', data.returnValues.cumulativeLikes);
+
+		// update label
+		var $likeButtonContainer = $container.find('li.likeButtons').first();
+		$likeButtonContainer.children('span').first().text(data.returnValues.cumulativeLikes);
+
+		// mark button as active
+		var $buttons = $likeButtonContainer.children('img');
+		var $buttonLike = $buttons.first().removeClass('active');
+		var $buttonDislike = $buttons.last().removeClass('active');
+
+		if (data.returnValues.isLiked) {
+			$buttonLike.addClass('active');
+		}
+		else if (data.returnValues.isDisliked) {
+			$buttonDislike.addClass('active');
+		}
 	}
-}
+});
